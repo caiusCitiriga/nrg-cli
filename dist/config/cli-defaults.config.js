535348b9f7322636b556d0e58115a7ae83e29877
Caius Citiriga <nbkmaxine@gmail.com>
2018-03-10 21:03:05 +0100

    updated schemas

--- dist/config/cli-defaults.config.js
+++ dist/config/cli-defaults.config.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CLI_CONF_FILENAME = '.energy.cli.json';
 exports.CLI_DEFAULTS = {
-    $schema: 'https://raw.githubusercontent.com/caiusCitiriga/nrg-cli/4324ea9ef7c8e2a3d2a4cab75f6d74b13298d4c0/src/config/cli-conf.schema.json',
+    $schema: 'https://raw.githubusercontent.com/caiusCitiriga/nrg-cli/38bd13fb9fd8054793460851cf4b8c75635c3779/src/config/cli-conf.schema.json',
     srcFolder: 'src',
     defaultExt: 'ts',
     additionalTypes: [],
--- src/config/cli-defaults.config.ts
+++ src/config/cli-defaults.config.ts
@@ -3,7 +3,7 @@ import { IEnergyCLIConf } from '../interfaces/energy-cli-conf.interface';
 export const CLI_CONF_FILENAME = '.energy.cli.json';
 
 export const CLI_DEFAULTS: IEnergyCLIConf = {
-    $schema: 'https://raw.githubusercontent.com/caiusCitiriga/nrg-cli/4324ea9ef7c8e2a3d2a4cab75f6d74b13298d4c0/src/config/cli-conf.schema.json',
+    $schema: 'https://raw.githubusercontent.com/caiusCitiriga/nrg-cli/38bd13fb9fd8054793460851cf4b8c75635c3779/src/config/cli-conf.schema.json',
     srcFolder: 'src',
     defaultExt: 'ts',
     additionalTypes: [],
